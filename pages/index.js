a9d06aebd6d58959497bfc8ed07d84f8d63eae0d
uanoob <usnet@ukr.net>
2019-12-11 15:25:02 +0200

    Added pagination with query params

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,29 @@
 import fetch from 'isomorphic-fetch';
 import Error from 'next/error';
+import Link from 'next/link';
 import Layout from '../components/Layout';
 import StoryList from '../components/StoryList';
 
 class Index extends React.Component {
-  static async getInitialProps() {
+  static async getInitialProps({ req, res, query }) {
     let stories = [];
+    let page = Number(query.page) || 1;
     try {
-      const res = await fetch('https://node-hnapi.herokuapp.com/news?page=1');
+      const res = await fetch(
+        `https://node-hnapi.herokuapp.com/news?page=${page}`,
+      );
       stories = await res.json();
     } catch (err) {
       console.log(err);
       stories = [];
     }
     return {
+      page,
       stories,
     };
   }
   render() {
-    const { stories } = this.props;
+    const { page, stories } = this.props;
 
     return stories.length === 0 ? (
       <Error statusCode={503} />
@@ -28,6 +33,23 @@ class Index extends React.Component {
         description={'A Hacker News clone made with Next.js'}
       >
         <StoryList stories={stories} />
+        <footer>
+          <Link href={`/?page=${page + 1}`}>
+            <a>Next Page ({page + 1})</a>
+          </Link>
+        </footer>
+        <style jsx>
+          {`
+            footer {
+              padding: 1em;
+            }
+            footer a {
+              font-weight: bold;
+              color: black;
+              text-decoration: none;
+            }
+          `}
+        </style>
       </Layout>
     );
   }
